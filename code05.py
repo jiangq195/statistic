74714ccc78a06672937267523e1c3d1da1ab189a
test001 <jiangq113@163.com>
2020-04-09 15:16:47 +0800

    4-9

--- code05.py
+++ code05.py
@@ -31,9 +31,11 @@ def create_data():
     # 返回数据集和每个维度的名称
     return datasets, labels
 
+
 datasets, labels = create_data()
 train_data = pd.DataFrame(datasets, columns=labels)
 print(train_data)
+
 
 # 熵
 def calc_ent(datasets):
@@ -44,9 +46,10 @@ def calc_ent(datasets):
         if label not in label_count:
             label_count[label] = 0
         label_count[label] += 1
-    ent = -sum([(p/data_length) * log(p/data_length, 2) for p in label_count.values()])
+    ent = -sum([(p / data_length) * log(p / data_length, 2) for p in label_count.values()])
 
     return ent
+
 
 # 经验条件熵
 def cond_ent(datasets, axis=0):
@@ -58,13 +61,15 @@ def cond_ent(datasets, axis=0):
             feature_sets[feature] = []
         feature_sets[feature].append(datasets[i])
 
-    cond_ent = sum([(len(p)/data_length) * calc_ent(p) for p in feature_sets.values()])
+    cond_ent = sum([(len(p) / data_length) * calc_ent(p) for p in feature_sets.values()])
 
     return cond_ent
+
 
 # 信息增益
 def info_gain(ent, cond_ent):
     return ent - cond_ent
+
 
 def info_gain_train(datasets):
     count = len(datasets[0]) - 1
@@ -78,6 +83,7 @@ def info_gain_train(datasets):
 
     best_ = max(best_feature, key=lambda x: x[-1])
     return '特征({})的信息增益最大，选择为根节点特征'.format(labels[best_[0]])
+
 
 res = info_gain_train(np.array(datasets))
 print(res)
@@ -109,6 +115,7 @@ class Node:
 
         return self.tree[feature[self.feature]].predict(feature)
 
+
 class DTree:
     def __init__(self, epsilon=0.1):
         self.epsilon = epsilon
@@ -124,7 +131,7 @@ class DTree:
             if label not in label_count:
                 label_count[label] = 0
             label_count[label] += 1
-        ent = -sum([(p/data_length)*log(p/data_length, 2) for p in label_count.values()])
+        ent = -sum([(p / data_length) * log(p / data_length, 2) for p in label_count.values()])
 
         return ent
 
@@ -137,7 +144,7 @@ class DTree:
             if feature not in feature_sets:
                 feature_sets[feature] = []
             feature_sets[feature].append(datasets[i])
-        cond_ent = sum([(len(p)/data_length) * calc_ent(p) for p in feature_sets.values()])
+        cond_ent = sum([(len(p) / data_length) * calc_ent(p) for p in feature_sets.values()])
 
         return cond_ent
 
@@ -151,10 +158,10 @@ class DTree:
         ent = self.calc_ent(datasets)
         best_feature = []
         for c in range(count):
-            c_info_gain = self.info_gain(ent, cond_ent(datasets, axis=c))
+            c_info_gain = self.info_gain(ent, self.cond_ent(datasets, axis=c))
             best_feature.append((c, c_info_gain))
         # 比较大小
-        best_ = max(best_feature, lambda x: x[-1])
+        best_ = max(best_feature, key=lambda x: x[-1])
         return best_
 
     def train(self, train_data):
@@ -162,8 +169,101 @@ class DTree:
         input:数据集D(DataFrame格式),特征集A，阈值eta
         output:决策树T
         """
-        _, y_train, feature = train_data.iloc[:, :-1], train_data.iloc[:, -1], train_data.columns[:-1]
+        _, y_train, features = train_data.iloc[:, :-1], train_data.iloc[:, -1], train_data.columns[:-1]
         # 1.若D中实例属于同一类Ck，则T为单节点数，并将类Ck作为节点的类标记，返回T
-        
+        if len(y_train.value_counts()) == 1:
+            return Node(root=True, label=y_train.iloc[0])
+
+        # 2.若A为空，则T为单节点树，将D中实例树最大的类C可作为该节点的类标记，返回T
+        if len(features) == 0:
+            return Node(
+                root=True,
+                label=y_train.value_counts().sort_values(
+                    ascending=False
+                ).index[0]
+            )
+
+        # 3.计算最大信息增益，同5.1 Ag为信息增益最大的特征
+        max_feature, max_info_gain = self.info_gain_train(np.array(train_data))
+        max_feature_name = features[max_feature]
+
+        # 4.Ag的信息增益小于阈值eta，则置T为单节点树，并将D中是实例数最大的类Ck作为该节点的类标记，返回T
+        if max_info_gain < self.epsilon:
+            return Node(
+                root=True,
+                label=y_train.value_counts().sort_values(ascending=False).index[0]
+            )
+
+        # 5.构建Ag子集
+        node_tree = Node(
+            root=False,
+            feature_name=max_feature_name,
+            feature=max_feature
+        )
+
+        feature_list = train_data[max_feature_name].value_counts().index
+        for f in feature_list:
+            sub_train_df = train_data.loc[train_data[max_feature_name] == f].drop([max_feature_name], axis=1)
+
+            # 6,递归生成树
+            sub_tree = self.train(sub_train_df)
+            node_tree.add_node(f, sub_tree)
+
+        return node_tree
+
+    def fit(self, train_data):
+        self._tree = self.train(train_data)
+        return self._tree
+
+    def predict(self, X_test):
+        return self._tree.predict(X_test)
+
+
+datasets, labels = create_data()
+data_df = pd.DataFrame(datasets, columns=labels)
+dt = DTree()
+tree = dt.fit(data_df)
+
+print(tree)
+res = dt.predict(['老年', '否', '否', '一般'])
+print(res)
+
+ddd = {'label': None, 'feature': 2, 'tree': {'否': {'label': None, 'feature': 1,
+                                                   'tree': {'否': {'label': '否', 'feature': None, 'tree': {}},
+                                                            '是': {'label': '是', 'feature': None, 'tree': {}}}},
+                                             '是': {'label': '是', 'feature': None, 'tree': {}}}}
+
+def create_data():
+    iris = load_iris()
+    df = pd.DataFrame(iris.data, columns=iris.feature_names)
+    df['label'] = iris.target
+    df.columns = [
+        'sepal length', 'sepal width', 'petal length', 'petal width', 'label'
+    ]
+    data = np.array(df.iloc[:100, [0, 1, -1]])
+    return data[:, :2], data[:, -1]
+
+
+X, y = create_data()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import export_graphviz
+import graphviz
+
+clf = DecisionTreeClassifier()
+res = clf.fit(X_train, y_train)
+print(res)
+sco = clf.score(X_test, y_test)
+print(sco)
+
+tree_pic = export_graphviz(clf, out_file='mytree.pdf')
+with open('mytree.pdf') as f:
+    dot_graph = f.read()
+
+sss = graphviz.Source(dot_graph)
+print(sss)
+
+
 
 
